e2e418c3b7cbc66910711b0ad24cabd28fb8b084
l9l9n <l9l9ndos@gmail.com>
2023-08-26 20:36:12 +0600

    Try 2

--- bot_utils/states.py
+++ bot_utils/states.py
@@ -3,3 +3,4 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 
 class UserMessageState(StatesGroup):
     answer = State()
+    film_text = State()
--- redis_client.py
+++ redis_client.py
@@ -1,57 +1,61 @@
-from redis import asyncio as aioredis
-from config import REDIS_HOST, REDIS_PORT
+import aioredis
+from config import REDIS_HOST,REDIS_PORT
+
 
 
 
 class RedisClient:
-    def __init__(self) -> None:
+    def __init__(self):
         self.client = self._get_redis_client()
      
      
     @staticmethod    
     def _get_redis_client():
         try:
-            client = aioredis.Redis(host=REDIS_HOST, 
-                                    port=REDIS_PORT,
-                                    socket_timeout=5)
+            client = aioredis.Redis(host=REDIS_HOST,
+                    port=REDIS_PORT,socket_timeout=5)
             ping = client.ping()
             if ping:
                 return client
         except aioredis.AuthenticationError:
-            return "Ошибка"
-        
+            print('Ошибка')
+            raise aioredis.AuthenticationError
+        except Exception as e:
+            print(e)
+            return Exception
  
-    def _create_caching_key(self, user_tg_id):
+    def _create_caching_key(self,user_tg_id):
         return f"{user_tg_id}"
  
 
-    async def cache_user_data(self, user_tg_id, data):
+    async def cache_user_data(self,user_tg_id,data):
         key = self._create_caching_key(user_tg_id)
-        await self.client.hset(key, mapping=data)
+        await self.client.hmset(key,mapping=data)
         
-    async def get_user_data(self, user_tg_id):
+    async def get_user_data(self,user_tg_id):
         key = self._create_caching_key(user_tg_id)
         return await self.client.hgetall(key)
     
-    async def del_user_data(self, user_tg_id):
+    async def del_user_data(self,user_tg_id):
         key = self._create_caching_key(user_tg_id)
         await self.client.delete(key)
-
-    def _create_film_key(self, user_tg_id):
-        return f'{user_tg_id}'
+        
+        
+    def _create_film_key(self,user_tg_id):
+        return f"{user_tg_id}"
     
-
-    async def cache_user_film(self, user_tg_id, data):
+    async def cache_user_film(self,user_tg_id,data):
         key = self._create_film_key(user_tg_id)
-        return await self.client.hgetall(key, data)
-
-    async def get_user_film(self, user_tg_id):
+        await self.client.hmset(key,data)
+    
+    async def get_user_film(self,user_tg_id):
         key = self._create_film_key(user_tg_id)
+        aioredis.StrictRedis()
         return await self.client.hgetall(key)
     
-    async def delete_user_film(self, user_tg_id):
+    async def delete_user_film(self,user_tg_id):
         key = self._create_film_key(user_tg_id)
-        await self.client.delete(key)
-
+        await self.client.delete(key)    
+        
 
 redis_client = RedisClient()
